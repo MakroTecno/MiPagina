ae4b1e57a3055e0eab3443444a2a5974a1300b3b
MakroTecno <victorcalvache1@gmail.com>
2020-03-10 00:24:53 -0500

    Agregamos la carpeta css

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script src="js/jquery-3.4.1.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+
+<!-- HEADER -->
+<section class="header">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 logo">
+				<img src="img/logo.png">
+	        </div>
+	        <div class="col-12 menuinicio">
+				<ul class="nav nav-tabs justify-content-center">
+				  <li class="nav-item">
+				    <a class="nav-link" href="index.php">Principal</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="php/login/usuario.php">Plataforma</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="portafolio.php">Portafolio</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="contacto.php">Contactanos</a>
+				  </li>
+				</ul>
+	        </div>
+        </div>
+	</div>
+</section>
+<!-- CUERPO -->
+<section class="cuerpo">
+	<div class="container">
+
+	</div>
+</section>
+<!-- FOOTER -->
+<section class="footer">
+	<div class="container">
+
+	</div>
+
+
+
+
+
+
+	<script src="js/bootstrap.min.js"></script>
+</section>
+
+</body>
+</html>
