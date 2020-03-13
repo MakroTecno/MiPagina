7a5f3dacd303e9fd3eb0a7de61acff8fb25c3c1b
MakroTecno <victorcalvache1@gmail.com>
2020-03-12 23:27:08 -0500

    realize la paginacion

--- index.php
+++ index.php
@@ -1,60 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Muchas gracias a todos....</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<script src="js/jquery-3.4.1.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
-</head>
-<body>
-
-<!-- HEADER -->
-<section class="header">
-	<div class="container">
-		<div class="row">
-			<div class="col-12 logo">
-				<img src="img/logo.png">
-	        </div>
-	        <div class="col-12 menuinicio">
-				<ul class="nav nav-tabs justify-content-center">
-				  <li class="nav-item">
-				    <a class="nav-link" href="index.php">Principal</a>
-				  </li>
-				  <li class="nav-item">
-				    <a class="nav-link" href="php/login/usuario.php">Plataforma</a>
-				  </li>
-				  <li class="nav-item">
-				    <a class="nav-link" href="portafolio.php">Portafolio</a>
-				  </li>
-				  <li class="nav-item">
-				    <a class="nav-link" href="contacto.php">Contactanos</a>
-				  </li>
-				</ul>
-	        </div>
-        </div>
-	</div>
-</section>
-<!-- CUERPO -->
-<section class="cuerpo">
-	<div class="container">
-<h1>EL TITULO</h1>
-<p>HOLA A TODOS</p>
-
-	</div>
-</section>
-<!-- FOOTER -->
-<section class="footer">
-	<div class="container">
-
-	</div>
-
-
-
-
-
-
-	<script src="js/bootstrap.min.js"></script>
-</section>
-
-</body>
-</html>
+<?php 
+header('Content-Type: application/json');
+	include("php/conexion_mysql.php");
+	$datosql = "SELECT * FROM productos";
+	$datosql2 = mysqli_query($conectar,$datosql);
+	$total_count = $datosql2->num_rows;
+	$row_count = 5;
+	$page = 1;
+	$pagina = ($page-1)*$row_count;
+	$sql = "SELECT * FROM productos LIMIT $pagina, $row_count";
+	$sql2 = mysqli_query($conectar, $sql);
+	while ($rows = mysqli_fetch_array($sql2)){
+	$page_count = ceil($total_count/$row_count);
+	$pagarray = array('page_count'=>$page_count,
+					  'total_count'=>$total_count,
+					  'row'=>array($rows['codigo'],$rows['descripcion'],$rows['vunitario']),
+					  'row2'=>array($rows['codigo'],$rows['descripcion'],$rows['vunitario']),
+					  'page'=>$page,
+					  'row_count'=>$row_count);
+	}
+	
+	
+print_r($pagarray);
+?>
